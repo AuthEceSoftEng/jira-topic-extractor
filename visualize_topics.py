c013136c3d6d7684c0d6e592da3805a4878c5b24
Dimitris Nastos <dnn0363@gmail.com>
2023-01-27 01:00:25 +0200

    Update visualize_topics.py

--- visualize_topics.py
+++ visualize_topics.py
@@ -1,10 +1,10 @@
 from bertopic import BERTopic
 import pymongo
-from properties import database_host_and_port, model_name, model_save_folder
+from properties import database_host_and_port, project_name, model_save_folder
 client = pymongo.MongoClient(database_host_and_port)
 db = client["jidata"]
 # Get the issues of the project
-issues = [issue for issue in db["issues"].find({"projectname": model_name}, {"summary": 1, "description": 1, "assignee": 1}).sort("_id")]
+issues = [issue for issue in db["issues"].find({"projectname": project_name}, {"summary": 1, "description": 1, "assignee": 1}).sort("_id")]
 # Get the texts of the issues (concatenation between the title and the description)
 issue_texts = [issue.get("summary", "") + " " + issue.get("description", "") for issue in issues]
 topic_model = BERTopic.load(model_save_folder+model_name+".h5")
