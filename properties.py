82d2d8e3ae2d351b8d88c2d9cf9837e38cdd3e65
Dimitris Nastos <dnn0363@gmail.com>
2023-01-27 00:53:24 +0200

    Update properties.py

--- properties.py
+++ properties.py
@@ -1,3 +1,3 @@
 database_host_and_port = "mongodb://USERNAME:PASSWORD@HOSTNAME:PORT/"  # change this to the hostname and port of your database
-model_save_folder='PATH_TO_SAVE_FOLDER"#set the path to the folder the BERTopic models are or will be stored. 
-model_path="PATH_TO_MODEL"#set the path to the BERTopic model you want to visualize
+model_save_folder='PATH_TO_SAVE_FOLDER"#set the path to the folder the BERTopic models are or will be stored
+model_name="NAME_OF_THE_MODEL"#set the name of the BERTopic model you want to visualize
