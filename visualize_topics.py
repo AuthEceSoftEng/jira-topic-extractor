8c5481127d8644617be4b38a09bc1571c482653c
Dimitris Nastos <dnn0363@gmail.com>
2023-01-27 00:58:49 +0200

    Update visualize_topics.py

--- visualize_topics.py
+++ visualize_topics.py
@@ -1,13 +1,11 @@
 from bertopic import BERTopic
 import pymongo
-client = pymongo.MongoClient('mongodb://user:password@host:port/')
+from properties import database_host_and_port, model_name, model_save_folder
+client = pymongo.MongoClient(database_host_and_port)
 db = client["jidata"]
-# Get all projects
-projects = [project["key"] for project in db["projects"].find({"key": "FTPSERVER"}, {"key": 1})]
-for project in projects:
-        # Get the issues of the project
-        issues = [issue for issue in db["issues"].find({"projectname": project}, {"summary": 1, "description": 1, "assignee": 1}).sort("_id")]
-        # Get the texts of the issues (concatenation between the title and the description)
-        issue_texts = [issue.get("summary", "") + " " + issue.get("description", "") for issue in issues]
-topic_model = BERTopic.load("PATH_TO_MODEL"+"FTPSERVER.h5")
+# Get the issues of the project
+issues = [issue for issue in db["issues"].find({"projectname": model_name}, {"summary": 1, "description": 1, "assignee": 1}).sort("_id")]
+# Get the texts of the issues (concatenation between the title and the description)
+issue_texts = [issue.get("summary", "") + " " + issue.get("description", "") for issue in issues]
+topic_model = BERTopic.load(model_save_folder+model_name+".h5")
 topic_model.visualize_documents(issue_texts, hide_document_hover=True)
