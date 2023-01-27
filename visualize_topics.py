f489e483ad62e7cfab40483c5df3437769bc520f
Dimitris Nastos <dnn0363@gmail.com>
2023-01-27 12:36:09 +0200

    Update visualize_topics.py

--- visualize_topics.py
+++ visualize_topics.py
@@ -7,5 +7,7 @@ db = client["jidata"]
 issues = [issue for issue in db["issues"].find({"projectname": project_name}, {"summary": 1, "description": 1, "assignee": 1}).sort("_id")]
 # Get the texts of the issues (concatenation between the title and the description)
 issue_texts = [issue.get("summary", "") + " " + issue.get("description", "") for issue in issues]
+# Load the BERTopic model which corresponds to this project
 topic_model = BERTopic.load(model_save_folder+project_name+".h5")
+# create visualization of the topics
 topic_model.visualize_documents(issue_texts, hide_document_hover=True)
